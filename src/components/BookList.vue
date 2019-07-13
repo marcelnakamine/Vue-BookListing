c2527b537ec846862b5fd4e0c21b230d279a1d87
Marcel <marcel.nakamine@outlook.com>
2019-07-12 22:39:21 -0300

    Creating a BookForm component

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <book-form @addBook='appendBook'/>
     <ul>
-      <book-item v-for="book in books" :book="book" :key="book.id">
-      </book-item>
+      <book-item v-for="book in books" :book="book" :key="book.id" />
     </ul>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
   export default {
     name: 'BookList',
     components: {
-      BookItem
+      BookItem,
+      BookForm
     },
     data() {
       return {
@@ -23,6 +25,11 @@ import BookItem from './BookItem';
           {title: 'American Gods', author: 'Neil Gaiman'},
           {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
         ]
+      }
+    },
+    methods: {
+      appendBook(bookTitle, bookAuthor) {
+        this.books.push({ title: bookTitle, author: bookAuthor })
       }
     }
   }
