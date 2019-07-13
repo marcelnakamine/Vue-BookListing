b7e08a2f6c52a77b46caddbb1f6c577af1e92057
Marcel <marcel.nakamine@outlook.com>
2019-07-12 21:43:24 -0300

    Connecting BookItem and BookLIst

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,16 +2,19 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.id">
-        {{ book.title }}:{{ book.author }}
-      </li>
+      <book-item v-for="book in books" :book="book" :key="book.id">
+      </book-item>
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from './BookItem';
   export default {
     name: 'BookList',
+    components: {
+      BookItem
+    },
     data() {
       return {
         title: 'All Books',
